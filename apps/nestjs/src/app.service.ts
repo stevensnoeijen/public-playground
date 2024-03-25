2f432789340239bc06bc3376bf44001e816c2abc
Steven Snoeijen <stevensnoeijen@gmail.com>
2024-03-25 23:10:04 +0100

    add localstack to node-nestjs

--- apps/nestjs/src/app.service.ts
+++ apps/nestjs/src/app.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { client } from './db';
+import * as AWS from '@aws-sdk/client-s3';
 
 @Injectable()
 export class AppService {
+  readonly s3: AWS.S3;
+
+  constructor() {
+    this.s3 = new AWS.S3({
+      endpoint: 'http://localhost:4566',
+      region: 'us-east-1',
+    });
+  }
+
   async getHello() {
-    const result = await client.query('SELECT NOW() as now');
-    return 'Hello World! ' + result.rows[0].now;
+    const dbResult = await client.query('SELECT NOW() as now');
+    const buckets = await this.s3.listBuckets();
+
+    return (
+      'Hello World! it is' +
+      dbResult.rows[0].now +
+      ' and there are ' +
+      (buckets.Buckets?.length ?? 0) +
+      ' buckets on localstack'
+    );
   }
 }
