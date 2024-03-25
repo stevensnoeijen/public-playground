54ea45f873195c0d2f077cca1d7ebfdc15dd2420
Steven Snoeijen <stevensnoeijen@gmail.com>
2024-03-25 22:34:25 +0100

    add pg test to nestjs app

--- apps/nestjs/src/app.service.ts
+++ apps/nestjs/src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { client } from './db';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello() {
+    const result = await client.query('SELECT NOW() as now');
+    return 'Hello World! ' + result.rows[0].now;
   }
 }
