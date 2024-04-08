ac65f14b93c7444c60eb114ad06a5405a323f7de
Steven Snoeijen <stevensnoeijen@gmail.com>
2024-04-08 19:04:03 +0000

    feat: simplify DrizzleService

--- apps/nestjs/src/drizzle/drizzle.service.ts
+++ apps/nestjs/src/drizzle/drizzle.service.ts
@@ -1,15 +1,11 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { NodePgDatabase } from "drizzle-orm/node-postgres";
+import { Inject, Injectable } from '@nestjs/common';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
 
 @Injectable()
 export class DrizzleService {
-    public readonly db: NodePgDatabase<typeof schema>;
-
-    constructor(
-        @Inject('DRIZZLE_SOURCE')
-        db: NodePgDatabase<typeof schema>,
-      ) {
-        this.db = db;
-      }
+  constructor(
+    @Inject('DRIZZLE_SOURCE')
+    public readonly db: NodePgDatabase<typeof schema>,
+  ) {}
 }
