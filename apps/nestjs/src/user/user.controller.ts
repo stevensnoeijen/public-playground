16121868add9251cdc440612ecf9a2b3f1e5f1ca
Steven Snoeijen <stevensnoeijen@gmail.com>
2024-04-08 19:04:46 +0000

    feat: add user create

--- apps/nestjs/src/user/user.controller.ts
+++ apps/nestjs/src/user/user.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { DrizzleService } from '../drizzle/drizzle.service';
+import { CreateUserDto } from './dto/createUser.dto';
+import { UserTable } from 'src/drizzle/schema';
 
 @Controller('users')
 export class UserController {
-  constructor(
-    private readonly drizzleService: DrizzleService,
-  ) {}
+  constructor(private readonly drizzleService: DrizzleService) {}
 
   @Get()
   async getAll() {
     const users = await this.drizzleService.db.query.UserTable.findMany();
     return users;
+  }
+
+  @Post()
+  async createOne(@Body() body: CreateUserDto) {
+    const user = await this.drizzleService.db
+      .insert(UserTable)
+      .values({
+        email: body.email,
+      })
+      .returning();
+    return user;
   }
 }
