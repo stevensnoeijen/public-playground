6d938d962f4dc1b173876e85f2ca85d817999dd7
Steven Snoeijen <2750503+stevensnoeijen@users.noreply.github.com>
2024-05-05 18:46:14 +0000

    add codecov workflow

--- apps/nestjs/src/app.controller.spec.ts
+++ apps/nestjs/src/app.controller.spec.ts
@@ -1,22 +1,28 @@
-import { Test, type TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { client } from './db';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
+    const app = await Test.createTestingModule({
       providers: [AppService],
+      controllers: [AppController],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appController = app.get(AppController);
   });
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
+  });
+
+  // should move to global teardown
+  afterAll(async () => {
+    await client.end();
   });
 });
--- apps/nestjs/src/app.controller.ts
+++ apps/nestjs/src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
-import type { AppService } from './app.service';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
