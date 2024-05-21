35721cd9dbb30937ca495e2460b1311ed5e375f6
stevensnoeijen <2750503+stevensnoeijen@users.noreply.github.com>
2024-05-21 20:15:46 +0000

    fix unit-test action

--- apps/nestjs/src/app.service.ts
+++ apps/nestjs/src/app.service.ts
@@ -1,7 +1,7 @@
 import * as AWS from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
 import { sql } from 'drizzle-orm';
-import type { DrizzleService } from './drizzle/drizzle.service';
+import { DrizzleService } from './drizzle/drizzle.service';
 
 @Injectable()
 export class AppService {
